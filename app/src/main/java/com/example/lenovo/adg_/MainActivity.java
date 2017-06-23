1ad23ad67a62bb6e31795f5daaeef4c33fb7ad47
Allan Bastian <allan.batian9.ab@gmail.com>
2017-06-23 19:23:21 +0530

    Fixed bugs

--- app/src/main/java/com/example/lenovo/adg_/MainActivity.java
+++ app/src/main/java/com/example/lenovo/adg_/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.lenovo.adg_;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("ADG Internal");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.attendance: return true;
+            case R.id.freeSlot: return true;
+            case R.id.projects: return true;
+            case R.id.events: return true;
+            case R.id.meetings: return true;
+            case R.id.work: Intent intent = new Intent(this, Work.class);
+                            startActivity(intent);
+                            return true;
+            default: return false;
+        }
     }
 }
